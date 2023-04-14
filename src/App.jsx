d84414e99a238858508c55b3843804a1085e70e6
arbitrata <andrewmbugua76@gmail.com>
2023-04-14 10:16:26 +0300

    site set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="text-[30px] text-center text-blue-300 w-screen h-screen">
+     Welcome to the digital tech pioneers :)
+    </div>
+  )
+}
+
+export default App
